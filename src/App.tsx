7c94b63d135dd525a76c9c9716f82762e469e869
Grace de la Mora <grace.g.delamora@gmail.com>
2021-09-22 22:28:18 -0400

    Updated landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { ReactComponent as Logo } from './img/logo.svg';
 class App extends React.Component {
   render() {
     return <>
-      <Logo style={{height: '90vh'}} />
+      <Logo className="coming-soon-svg" />
+      <h1 className="coming-soon-text text-center">Opening<br />September 27</h1>
     </>;
   }
 }
